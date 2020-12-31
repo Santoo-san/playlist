842b17248de054fb5c120f03ee62f47ca966e5fb
Hans Kool <Hansjekool@hotmail.com>
2020-12-31 11:39:48 +0100

    Adds routing an d styling

---
+++ src/components/AboutUs.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1> About Us</h1>
+      <p>Sla jouw favo playlist op!</p>
+    </div>
+  );
+}
+
+export default AboutUs;
